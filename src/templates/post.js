ac40b557015fc552a316d1803d9ffaa49df90b1b
Sheng Zhong <shengzhong@ShengdeMBP.fritz.box>
2019-02-01 11:38:23 +0100

    20190201

--- src/templates/post.js
+++ src/templates/post.js
@@ -10,6 +10,35 @@ import TagList from '../components/TagList'
 import PostLinks from '../components/PostLinks'
 import PostDate from '../components/PostDate'
 import SEO from '../components/SEO'
+import styled from 'styled-components'
+
+import { DiscussionEmbed } from 'disqus-react'
+import { 
+  FacebookShareButton,
+  TwitterShareButton,
+  EmailShareButton, 
+  
+  FacebookIcon,
+  TwitterIcon,
+  EmailIcon, 
+} from 'react-share'
+
+const Comment = styled.div`
+  margin: 0 auto;
+  max-width: ${props => props.theme.sizes.maxWidthCentered};
+`
+
+const Share = styled.div`
+  margin: 0 auto;
+  max-width: ${props => props.theme.sizes.maxWidthCentered};
+  display: flex;
+  margin-bottom: 2em;
+`
+
+const ShareButton = styled.div`
+  cursor:pointer;
+  margin-right: 0.5em
+`
 
 const PostTemplate = ({ data, pageContext }) => {
   const {
@@ -25,6 +54,12 @@ const PostTemplate = ({ data, pageContext }) => {
   const previous = pageContext.prev
   const next = pageContext.next
 
+  const disqusShortname = "yuanmingju";
+  const disqusConfig = {
+    identifier: slug,
+    title: title,
+  };
+
   return (
     <Layout>
       <Helmet>
@@ -38,8 +73,46 @@ const PostTemplate = ({ data, pageContext }) => {
         {tags && <TagList tags={tags} />}
         <PostDate date={publishDate} />
         <PageBody body={body} />
+        <Share>
+          <ShareButton>
+          <FacebookShareButton
+            url={slug}
+            quote={title}
+            className="share-button">
+            <FacebookIcon
+              size={32}
+              round />
+          </FacebookShareButton>
+          </ShareButton>
+          <ShareButton>
+          <TwitterShareButton
+            url={slug}
+            title={title}
+            className="share-button">
+            <TwitterIcon
+              size={32}
+              round />
+          </TwitterShareButton>
+          </ShareButton>
+          <ShareButton>
+          <EmailShareButton
+            url={slug}
+            subject={title}
+            body="body"
+            className="share-button">
+            <EmailIcon
+              size={32}
+              round />
+          </EmailShareButton>
+          </ShareButton>
+        </Share>
       </Container>
       <PostLinks previous={previous} next={next} />
+      <Container>
+        <Comment>
+          <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+        </Comment>
+      </Container>  
     </Layout>
   )
 }
